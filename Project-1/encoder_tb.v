08b4ca4b904ee0cce22d2fd0686d8a34b29f40b4
Anusha-1214 <242ta04252@ashokacollege.in>
2026-08-02 21:36:48 +0530

    Add files via upload

---
+++ Project-1/encoder_tb.v
@@ -0,0 +1,45 @@
+`timescale 1ns/1ps
+
+module encoder_tb;
+
+reg [7:0] in;
+wire [2:0] out;
+
+encoder uut (
+    .in(in),
+    .out(out)
+);
+
+initial begin
+
+    $display("Input\t\tOutput");
+
+    in = 8'b00000001; #10;
+    $display("%b\t%b", in, out);
+
+    in = 8'b00000010; #10;
+    $display("%b\t%b", in, out);
+
+    in = 8'b00000100; #10;
+    $display("%b\t%b", in, out);
+
+    in = 8'b00001000; #10;
+    $display("%b\t%b", in, out);
+
+    in = 8'b00010000; #10;
+    $display("%b\t%b", in, out);
+
+    in = 8'b00100000; #10;
+    $display("%b\t%b", in, out);
+
+    in = 8'b01000000; #10;
+    $display("%b\t%b", in, out);
+
+    in = 8'b10000000; #10;
+    $display("%b\t%b", in, out);
+
+    $finish;
+
+end
+
+endmodule
